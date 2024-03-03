bbe6f02ec7899927db388cd6b2690d9a576187c6
Jo Ito <sqrtnull@gmail.com>
2024-03-03 21:12:51 +0900

    forgot to add obsidian

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,19 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "nexus",
+				path = "~/vaults/Nexus",
+			},
+		},
+	},
+}
+-- vim: ts=4 sts=4 sw=4 et
