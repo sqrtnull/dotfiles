eb62645ca3df45c88c77780fe6b1f74ce1ef4ea8
Jo Ito <sqrtnull@gmail.com>
2025-04-24 23:44:55 +0900

    fix: looking into wezterm again

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = 'Catppuccin Mocha'
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+    left=0,
+    right=0,
+    top=0,
+    bottom=0,
+}
+
+return config
