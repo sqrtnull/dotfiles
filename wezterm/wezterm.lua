94a8b6bf8e1c35731411bd4a6cedcf4acdf2a153
Jo Ito <sqrtnull@gmail.com>
2025-04-24 23:46:38 +0900

    fix: add font size as well

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,6 +5,8 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'Catppuccin Mocha'
 
+config.font_size = 18
+
 config.hide_tab_bar_if_only_one_tab = true
 
 config.window_padding = {
