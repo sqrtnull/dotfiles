c828a7c009b4fb46b8aeb7d69f9fd2b506b6872f
sqrtnull <sqrtnull@gmail.com>
2025-08-17 14:54:09 +0900

    feat: add number and relative number back

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,6 +24,8 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.scrolloff = 8
 vim.opt.conceallevel = 1
